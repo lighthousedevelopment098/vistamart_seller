49916fd56a2f013c8f5862bdb37054f5e0d4de0f
Harryexe74 <junaidmahmood141@gmail.com>
2024-11-19 11:58:32 +0500

    product update

--- src/components/FormInput/FileUpload.jsx
+++ src/components/FormInput/FileUpload.jsx
@@ -1,3 +1,30 @@
+// import React from "react";
+
+// const FileUpload = ({ name, label, accept, onChange }) => (
+//   <div className="form-group">
+//     <div className="title-color mb-2 d-flex gap-1 align-items-center">
+//       {label}
+//     </div>
+//     <div className="custom-file text-left">
+//       <input
+//         type="file"
+//         name={name}
+//         id={`${name}Input`}
+//         className="custom-file-input image-input"
+//         accept={accept}
+//         onChange={onChange}
+//       />
+//       <label className="custom-file-label" htmlFor={`${name}Input`}>
+//         Upload {label ? label.toLowerCase() : "file"} {/* Check label */}
+//       </label>
+//     </div>
+//   </div>
+// );
+
+// export default FileUpload;
+
+
+
 import React from "react";
 
 const FileUpload = ({ name, label, accept, onChange }) => (
@@ -15,7 +42,7 @@ const FileUpload = ({ name, label, accept, onChange }) => (
         onChange={onChange}
       />
       <label className="custom-file-label" htmlFor={`${name}Input`}>
-        Upload {label ? label.toLowerCase() : "file"} {/* Check label */}
+        Upload {label.toLowerCase()}
       </label>
     </div>
   </div>
